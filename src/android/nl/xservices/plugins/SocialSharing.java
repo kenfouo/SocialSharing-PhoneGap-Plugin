eb1bd360af6b8ea16f94029daad93595a4ab039e
kenfouo <37112508+kenfouo@users.noreply.github.com>
2026-02-11 18:59:11 +0100

    Refactor getActivity method for intent handling
    Updated activity list query to use MATCH_DEFAULT_ONLY and added logging for the number of activities found.

--- src/android/nl/xservices/plugins/SocialSharing.java
+++ src/android/nl/xservices/plugins/SocialSharing.java
@@ -676,7 +676,15 @@ public class SocialSharing extends CordovaPlugin {
 
   private ActivityInfo getActivity(final CallbackContext callbackContext, final Intent shareIntent, final String appPackageName, final String appName) {
     final PackageManager pm = webView.getContext().getPackageManager();
-    List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+    
+    //List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+    //List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+        
+    List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, PackageManager.MATCH_DEFAULT_ONLY);
+    
+    // Affiche le nombre d'activités trouvées 
+    Log.d("SharePlugin", "Nombre d'activités trouvées : " + activityList.size());
+    
     for (final ResolveInfo app : activityList) {
       if ((app.activityInfo.packageName).contains(appPackageName)) {
         if (appName == null || (app.activityInfo.name).contains(appName)) {
